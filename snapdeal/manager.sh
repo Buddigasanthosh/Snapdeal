97fc570f694bb7ae6d220eb72e841c5e9585e454
Buddigasanthosh <90147567+Buddigasanthosh@users.noreply.github.com>
2021-09-07 19:30:23 +0530

    Update manager.sh

--- snapdeal/manager.sh
+++ snapdeal/manager.sh
@@ -6,6 +6,14 @@ echo $a
 done
 
 echo "ForLoop Demo Over"
-
-
-		
+`expr 3 + 2`
+echo "if + read"
+a=789
+b=578
+read -p  "Enter $a value"
+read -p  "Enter $b value"
+if [ $a -lt $b ]
+then
+echo "$a is less than $b" 
+else
+echo "$b less than $a "
